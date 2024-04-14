c9fff935eb71dc3cc8f0bf3264cda4f8c70f3af6
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-04-14 20:59:36 +0900

    read until No.10

--- 02-code-project-organization/10-type-embedding/main.go
+++ 02-code-project-organization/10-type-embedding/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"io"
+	"sync"
+)
+
+type InMem struct {
+	mu sync.Mutex
+	m  map[string]int
+}
+
+func New() *InMem {
+	return &InMem{m: make(map[string]int)}
+}
+
+func (i *InMem) Get(key string) (int, bool) {
+	i.mu.Lock()
+	v, contains := i.m[key]
+	i.mu.Unlock()
+
+	return v, contains
+}
+
+type Logger struct {
+	io.WriteCloser
+}
+
+func main() {
+	l := Logger{}
+	_, _ = l.Write([]byte("byte"))
+	_ = l.Close()
+}
