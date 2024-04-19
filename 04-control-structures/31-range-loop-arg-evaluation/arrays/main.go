2598cf1ddf21d22850a92bc144dfde85ec67cbd5
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-04-19 18:29:54 +0900

    read until No.33

---
+++ 04-control-structures/31-range-loop-arg-evaluation/arrays/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	listing1()
+	listing2()
+	listing3()
+}
+
+func listing1() {
+	a := [3]int{0, 1, 2}
+	for i, v := range a {
+		a[2] = 10
+		if i == 2 {
+			fmt.Println(v)
+		}
+	}
+}
+
+func listing2() {
+	a := [3]int{0, 1, 2}
+	for i := range a {
+		a[2] = 10
+		if i == 2 {
+			fmt.Println(a[2])
+		}
+	}
+}
+
+func listing3() {
+	a := [3]int{0, 1, 2}
+	for i, v := range &a {
+		a[2] = 10
+		if i == 2 {
+			fmt.Println(v)
+		}
+	}
+}
---
+++ 04-control-structures/31-range-loop-arg-evaluation/concepts/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1 := []int{0, 1, 2}
+	for range s1 {
+		s1 = append(s1, 10)
+	}
+
+	s2 := []int{0, 1, 2}
+	for i := 0; i < len(s2); i++ {
+		s2 = append(s2, 10)
+	}
+
+	fmt.Println(s1, s2)
+}
