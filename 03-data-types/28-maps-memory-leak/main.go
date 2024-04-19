30d904393ac499fd0cecb1e5569c83dac471a8ac
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-04-19 10:34:41 +0900

    read until No.28

---
+++ 03-data-types/28-maps-memory-leak/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	n := 1_000_000
+	m := make(map[int]*[128]byte)
+	printAlloc()
+
+	for i := 0; i < n; i++ {
+		m[i] = randBytes()
+	}
+	printAlloc()
+
+	for i := 0; i < n; i++ {
+		delete(m, i)
+	}
+
+	runtime.GC()
+	printAlloc()
+	runtime.KeepAlive(m)
+}
+
+func randBytes() *[128]byte {
+	return &[128]byte{}
+}
+
+func printAlloc() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("%d MB\n", m.Alloc/1024/1024)
+}
