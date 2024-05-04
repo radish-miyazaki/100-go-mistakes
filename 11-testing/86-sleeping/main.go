c00d97803eccbc4463eddc0e50dd198e4d1f9567
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-05-04 21:25:18 +0900

    read No.86

---
+++ 11-testing/86-sleeping/main.go
@@ -0,0 +1,30 @@
+package main
+
+type Handler struct {
+	n         int
+	publisher publisher
+}
+
+type publisher interface {
+	Publish([]Foo)
+}
+
+type Foo struct{}
+
+func (h Handler) getBestFoo(someInputs int) Foo {
+	foos := getFoos(sampleInputs)
+	best := foos[0]
+
+	go func() {
+		if len(foos) > h.n {
+			foos = foos[:h.n]
+		}
+		h.publisher.Publish(foos)
+	}()
+
+	return best
+}
+
+func getFoos(_ int) []Foo {
+	return make([]Foo, 100)
+}
---
+++ 11-testing/86-sleeping/main_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+type publisherMock1 struct {
+	mu  sync.RWMutex
+	got []Foo
+}
+
+func (p *publisherMock1) Publish(got []Foo) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.get = got
+}
+
+func (p *publisherMock1) Get() []Foo {
+	p.mu.RLock()
+	defer p.mu.Unlock()
+	return p.got
+}
+
+func TestGetBestFoo1(t *testing.T) {
+	mock := publisherMock1{}
+	h := Handler{
+		publisher: &mock,
+		n:         2,
+	}
+
+	foo := h.getBestFoo(42)
+	time.Sleep(10 * time.Millisecond)
+	published := mock.Get()
+
+	_ = foo
+	_ = published
+}
+
+func assert(t *testing.T, assertion func() bool, maxRetry int, waitTime time.Duration) {
+	for i := 0; i < maxRetry; i++ {
+		if assertion() {
+			return
+		}
+
+		time.Sleep(waitTime)
+	}
+	t.Fail()
+}
+
+type publisherMock2 struct {
+	ch chan []Foo
+}
+
+func (p *publisherMock2) Publish(got []Foo) {
+	p.ch <- got
+}
+
+func TestGetBestFoo2(t *testing.T) {
+	mock := publisherMock2{
+		ch: make(chan []Foo),
+	}
+	defer close(mock.ch)
+
+	h := Handler{
+		publisher: mock,
+		n:         2,
+	}
+	foo := h.getBestFoo(42)
+
+	if v := len(<-mock.ch); v != 2 {
+		t.Fatalf("expected: 2, got: %d", v)
+	}
+
+	_ = foo
+}
