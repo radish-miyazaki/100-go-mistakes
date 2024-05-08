9891ed9d20d646de056921608813d71c8081468e
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-05-08 14:49:10 +0900

    read No.76

---
+++ 10-standard-lib/76-time-after/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+)
+
+type Event struct{}
+
+func handle(Event) {}
+
+func consumer(ch <-chan Event) {
+	for {
+		select {
+		case event := <-ch:
+			handle(event)
+		case <-time.After(time.Hour):
+			log.Println("warning: no messages received")
+		}
+	}
+}
+
+func consumer2(ch <-chan Event) {
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Hour)
+		select {
+		case event := <-ch:
+			cancel()
+			handle(event)
+		case <-ctx.Done():
+			log.Println("warning: no message received")
+		}
+	}
+}
+
+func consumer3(ch <-chan Event) {
+	timerDuration := 1 * time.Hour
+	timer := time.NewTimer(timerDuration)
+
+	for {
+		timer.Reset(timerDuration)
+		select {
+		case event := <-ch:
+			handle(event)
+		case <-timer.C:
+			log.Println("warning: no message received")
+		}
+	}
+}
