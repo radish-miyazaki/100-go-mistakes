914705de4828edfc3a3f2f75b1837f38c2789932
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-05-10 14:38:54 +0900

    read No.81

---
+++ 10-standard-lib/81-default-http-client-server/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+	s := &http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: 500 * time.Millisecond,
+		ReadTimeout:       500 * time.Millisecond,
+		Handler:           http.TimeoutHandler(handler{}, time.Second, "foo"),
+	}
+
+	_ = s
+}
+
+type handler struct{}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
