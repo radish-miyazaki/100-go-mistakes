16abc10f21822dd7bd8c87f7e870be48849ba5f8
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-05-17 14:39:10 +0900

    read until No.60

--- 08-concurrency-fondations/60-contexts/main.go
+++ 08-concurrency-fondations/60-contexts/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/radish-miyazaki/100-go-mistakes/08-concurrency-fondations/60-contexts/flight"
@@ -19,4 +20,31 @@ func (h publishHandler) publishPosition(position flight.Position) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
 	defer cancel()
 	return h.pub.Publish(ctx, position)
+}
+
+type key string
+
+const isValidHostKey key = "isValidHost"
+
+func checkValid(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		validHost := r.Host == "acme"
+		ctx := context.WithValue(r.Context(), isValidHostKey, validHost)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+type Message struct{}
+
+func handler(ctx context.Context, ch chan Message) error {
+	for {
+		select {
+		case msg := <-ch:
+			// msg でなにか処理を行う
+			_ = msg
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
 }
